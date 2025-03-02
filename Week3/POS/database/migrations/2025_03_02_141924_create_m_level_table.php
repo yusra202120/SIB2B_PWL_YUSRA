d5ae2fcd0b74acc6c504355737a6f43baafe3673
yusra_ys <yusrayusuf811@gmail.com>
2025-03-02 22:13:24 +0700

    Menambahkan file migration

--- Week3/POS/database/migrations/2025_03_02_141924_create_m_level_table.php
+++ Week3/POS/database/migrations/2025_03_02_141924_create_m_level_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('m_lavel', function (Blueprint $table) {
+        Schema::create('m_level', function (Blueprint $table) {
             $table->id('level_id');
             $table->string('level_kode', 10)->unique();
             $table->string('level_nama', 100);
@@ -19,11 +19,12 @@ return new class extends Migration
         });
     }
 
+
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('m_lavel');
+        Schema::dropIfExists('m_level');
     }
 };
