da050d067b0ca212b419dd96e46188e0afa49435
yusra_ys <yusrayusuf811@gmail.com>
2025-03-10 16:52:30 +0700

    Retreiving Aggregrates

--- Week4/POS/resources/views/user.blade.php
+++ Week4/POS/resources/views/user.blade.php
@@ -7,17 +7,13 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
+                <th>Jumlah Pengguna</th>
             </tr>
-            <tr>
-                <td>{{ $data->user_id }}</td> <!-- $d ini mewakili data dari data dari tabel m_user karena seperti yang dilakukan tadi 'data' = $user dan $user itu UserModel yang isinya m_user -->
-                <td>{{ $data->username }}</td> <!-- selnjutnya tampilkan data di baris pertama dengan isi dari user_id di satu persatu sampe akhir dan terus sampai $data sudah ditampilakan -->
-                <td>{{ $data->nama }}</td>                
-                <td>{{ $data->level_id }}</td>    
-            </tr>            
+
+            <tr>             
+                <td>{{ $data }}</td>    
+            </tr>       
+        
         </table>
     </body>
 </html>
