660d1463f269e17e8c1f6aa92200af56021027cf
yusra_ys <yusrayusuf811@gmail.com>
2025-03-18 01:21:17 +0700

    Membuat form kemudian menyimpan data dalam database

--- Week5/POS/app/Http/Controllers/KategoriController.php
+++ Week5/POS/app/Http/Controllers/KategoriController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use App\DataTables\KategoriDataTable;
 
@@ -10,5 +11,20 @@ class KategoriController extends Controller
     public function index(KategoriDataTable $dataTable)
     {
         return $dataTable->render('kategori.index');
+    }
+
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+
+        return redirect('/kategori');
     }
 }
--- Week5/POS/routes/web.php
+++ Week5/POS/routes/web.php
@@ -65,3 +65,6 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::post('/kategori', [KategoriController::class, 'store']);
