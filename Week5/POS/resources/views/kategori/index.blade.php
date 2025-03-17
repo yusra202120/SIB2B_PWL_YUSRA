47715a8d99c666e706aa616ffca6d4fb1227e0cf
yusra_ys <yusrayusuf811@gmail.com>
2025-03-18 01:22:21 +0700

    Menambahkan button Add Kategori

--- Week5/POS/resources/views/kategori/index.blade.php
+++ Week5/POS/resources/views/kategori/index.blade.php
@@ -1,19 +1,24 @@
 @extends('layouts.app')
 
-{{-- Customize layout sections --}}
 @section('subtitle', 'Kategori')
-@section('content_header_title', 'Home')
-@section('content_header_subtitle', 'Kategori')
+@section('content_header_title', 'Kategori')
+@section('content_header_subtitle', 'Manage')
 
 @section('content')
-    <div class="container">
-        <div class="card">
-            <div class="card-header">Manage Kategori</div>
-            <div class="card-body">
-                {!! $dataTable->table() !!}
-            </div>
+<div class="container">
+    <div class="mb-3">
+        <a href="{{ url('/kategori/create') }}" class="btn btn-success">
+            <i class="fas fa-plus"></i> Add Kategori
+        </a>
+    </div>
+
+    {{-- Tempat untuk table DataTable --}}
+    <div class="card card-primary">
+        <div class="card-body">
+            {!! $dataTable->table(['class' => 'table table-bordered table-striped']) !!}
         </div>
     </div>
+</div>
 @endsection
 
 @push('scripts')
