2bd391a6d40db828fc1446db50ce701eadc8ad6f
yusra_ys <yusrayusuf811@gmail.com>
2025-03-24 19:51:11 +0700

    first commit JS-5 rev

---
+++ Week5_New/POS/database/migrations/2025_03_02_150011_create_t_penjualan_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id'); // Primary Key
+            $table->unsignedBigInteger('user_id'); // Foreign Key ke m_user
+            $table->string('pembeli', 50);
+            $table->string('penjualan_kode', 20)->unique();
+            $table->dateTime('penjualan_tanggal');
+            $table->timestamps();
+
+            // Foreign key ke tabel m_user
+            $table->foreign('user_id')->references('user_id')->on('m_user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan');
+    }
+};
