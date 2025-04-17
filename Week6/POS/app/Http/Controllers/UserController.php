9dd2a0520597df33e2b23b6d6ba50ae391ac12c8
yusra_ys <yusrayusuf811@gmail.com>
2025-04-18 00:08:22 +0700

    Hapus Data Ajax

--- Week6/POS/app/Http/Controllers/UserController.php
+++ Week6/POS/app/Http/Controllers/UserController.php
@@ -170,8 +170,37 @@ class UserController extends Controller
         return redirect('/');
     }
 
+        public function confirm_ajax(string $id)
+    {
+        $user = UserModel::find($id);
 
-    
+        return view('user.confirm_ajax', ['user' => $user]);
+    }
+
+
+
+        public function delete_ajax(Request $request, $id)
+    {
+        // cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status'  => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status'  => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+
+        return redirect('/');
+    }
+
 
 
     // Menampilkan halaman form tambah user
--- Week6/POS/routes/web.php
+++ Week6/POS/routes/web.php
@@ -52,6 +52,9 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);    // Menampilkan halaman form edit user Ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
 
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // Untuk tampilkan form konfirmasi delete user Ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // Untuk hapus data user Ajax
+    
     Route::delete('/{id}', [UserController::class, 'destroy']);     // menghapus data user
 });
 
