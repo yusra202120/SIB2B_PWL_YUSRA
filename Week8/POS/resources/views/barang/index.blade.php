3c80eed5fc52ef901faaaca75794a4bc2dbbe64a
yusra_ys <yusrayusuf811@gmail.com>
2025-04-22 00:24:18 +0700

    Implementasi Export File

--- Week8/POS/resources/views/barang/index.blade.php
+++ Week8/POS/resources/views/barang/index.blade.php
@@ -6,7 +6,7 @@
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-info mt-1">Import Barang</button>
-                <a class="btn btn-sm btn-primary mt-1" href="{{ url('barang/create') }}">Tambah</a>
+                <a href="{{ url('/barang/export_excel') }}" class="btn btn-primary"><i class="fa fa-file- excel"></i> Export Barang</a>                
                 <button onclick="modalAction('{{ url('/barang/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
             </div>
         </div>
--- Week8/POS/routes/web.php
+++ Week8/POS/routes/web.php
@@ -147,6 +147,7 @@ Route::middleware(['auth'])->group(function () {
 
         Route::get('/import', [BarangController::class, 'import']); // ajax upload excel
         Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+        Route::get('/export_excel', [BarangController::class, 'export_excel']); // export excel
 
     });
 });
