3fe535b98fb456e867efacba9db09e7fc46edbac
yusra_ys <yusrayusuf811@gmail.com>
2025-04-29 00:44:54 +0700

    Praktikum 2 – Membuat RESTful API Login

--- Week10/POS/routes/api.php
+++ Week10/POS/routes/api.php
@@ -14,4 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Http\Controllers\Api\LoginController;
+
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
